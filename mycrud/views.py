418e3a5b9f3adf16886d0b890e391f4fc71f8c41
AdityaPranata1702 <2011102441052@umkt.ac.id>
2022-12-14 18:29:11 +0800

    Api Sudah Tampil Di Halaman Home

--- mycrud/views.py
+++ mycrud/views.py
@@ -5,9 +5,24 @@ from posts.views import *
 
 def index(request):
     template_name = 'index.html'
+    url = "https://newsapi.org/v2/everything?q=Cricket&from=2022-12-14&sortBy=popularity&apiKey=1b15168c2d2f4d15896b3de6b7c8be90"
     
+    cricket_news = requests.get(url).json()
+    a = cricket_news['articles']
+    desc = []
+    title = []
+    img = []
+    
+    for i in range(len(a)):
+        f = a[i]
+        title.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+        
+    mylist = zip(title, desc, img)
     context = {
         'title' : 'HOME',
+        'mylist' : mylist,
     }
     return render(request, template_name, context)
 
